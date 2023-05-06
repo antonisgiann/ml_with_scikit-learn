a224adda3916437025bea6cf3a719437a37a48ed
carnivore7777 <antgiannoulopoulos@gmail.com>
2023-05-06 15:10:10 +0100

    Updating main script, improving outlier function

--- Regression/usa-real-estate-dataset/main.py
+++ Regression/usa-real-estate-dataset/main.py
@@ -23,7 +23,15 @@ od.download(PROJECT_URL, "../datasets")
 df = pd.read_csv(os.path.join(DATA_PATH, "realtor-data.csv"))
 # %%
 df.info()
-# Status seems irrelevant, prev_sold_date 71% missing values
+# %%
+df.describe()
+# From the above we can see we have a lot of outliers
+# %%
+df.describe(include='O').T
+# %%
+df.isnull().sum()
+
+# Status seems irrelevant , prev_sold_date 71% missing values
 df.drop(columns=["status", "prev_sold_date"], inplace=True)
 # %% 
 print(df.state.value_counts())
@@ -32,13 +40,6 @@ print(df.shape)
 df = df.loc[~df.state.isin(["South Carolina", "Tennessee", "Virginia", "New Jersey"]), :]
 print(df.shape)
 # %%
-df.describe()
-# From the above we can see we have a lot of outliers
-# %%
-df.describe(include='O').T
-# %%
-df.isnull().sum()
-# %%
 df.groupby('state').price.mean().sort_values(ascending=False).plot.bar()
 plt.title('Average Price of Real Estate for US states')
 plt.show()
@@ -46,32 +47,45 @@ df.groupby('city').price.mean().sort_values(ascending=False).head(20).plot.bar()
 plt.title('Average Price per city')
 plt.show()
 # %%
-# %% Trying to drop rows that have at least one null value
+# %% Drop rows that have at least one null value
 df_dropna_all = df.dropna()
-# %% To verify that we still have outliers
-sns.kdeplot(df_dropna_all["price"])
-# %% I will consider an outlier everything that has z score above 3.5
+# %% Removing outliers
 def find_outliers(col):
 
-    mean, std = col.describe()[["mean", "std"]]
-    z_score = col.map(lambda x: np.abs((mean-x)/std))
-    print(f"Found {(z_score > 3.5).sum()} outliers")
+    Q1, Q3 = col.describe()[["25%", "75%"]]
+    IQR = Q3 - Q1
+    high = Q3 + IQR * 1.5
+    low = Q1 - IQR * 1.5
+    is_outliar = col.map(lambda x: x > high or x < low)
+    print(f"Found {(is_outliar).sum()} outliers")
 
-    return z_score > 3
+    return is_outliar
 
 print(df_dropna_all.shape)
-df_dropna_all = df_dropna_all.loc[~find_outliers(df_dropna_all["price"]), :]
+for c in ["bed", "bath", "acre_lot", "house_size", "price"]:
+    print(f"checking {c}")
+    
+    df_dropna_all = df_dropna_all.loc[~find_outliers(df_dropna_all[c]), :]
 print(df_dropna_all.shape)
-sns.scatterplot(x=df_dropna_all["acre_lot"], y=df_dropna_all["price"])
+# %%
+sns.scatterplot(df_dropna_all, x="acre_lot", y="price", hue="state")
+plt.show()
+sns.scatterplot(df_dropna_all, x="house_size", y="price", hue="state")
+plt.show()
+corr_mat = df_dropna_all[["house_size", "bed", "acre_lot", "bath", "price"]].corr()
+mask = np.triu(np.ones_like(corr_mat)).astype(bool)
+mask = mask[1:, :-1]
+corr_mat = corr_mat.iloc[1:,:-1].copy()
+sns.heatmap(corr_mat, annot=True, fmt=".2f", cmap="crest", mask=mask)
+plt.show()
+sns.kdeplot(df_dropna_all["price"], fill=True, color="#0092A0")
+plt.show()
 # %% Normalize the target
 #df_dropna_all["price"] = stats.boxcox(df_dropna_all["price"])[0]
 #sns.kdeplot(df_dropna_all["price"], color="green", fill=True)
 # %%
-#sns.pairplot(df_dropna_all[["bed", "bath", "acre_lot", "house_size", "price"]])
-# %%
 X_dropna_all = df_dropna_all.drop(columns=["price"])
 y_dropna_all = df_dropna_all["price"].copy()
-
 # %% First lets try only with numerical columns
 X_dropna_all_num = df_dropna_all.select_dtypes(exclude=["object"])
 X_train_dropna_all_num, X_test_dropna_all_num, y_train_dropna_all, y_test_dropna_all = train_test_split(X_dropna_all_num, y_dropna_all, test_size=0.2, random_state=47)
@@ -92,7 +106,7 @@ print(np.sqrt(mean_squared_error(preds, y_test_dropna_oh)))
 # From the above looks like state and city add a lot of noise and hurt performance a lot
 # %%
 cat = CatBoostRegressor(random_state=47)
-cat.fit(X_train_dropna_all_num, y_train_dropna_all)
+cat.fit(X_train_dropna_all_num, y_train_dropna_all, verbose=0)
 preds = cat.predict(X_test_dropna_all_num)
 print(np.sqrt(mean_squared_error(preds, y_test_dropna_all)))
 # %%
